81a7c993924fb2beb527faa6269e49cc081e3be9
Seia-Soto <seia@outlook.kr>
2021-02-25 00:32:47 +0900

    Create new post

--- components/Extras/P.jsx
+++ components/Extras/P.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Text } from '@chakra-ui/react'
 
-const P = props => <Text {...props} />
+const P = props => <Text margin='9px 0' {...props} />
 
 export default P
